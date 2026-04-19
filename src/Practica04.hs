59ec300e9fcdcd6036fef6f788a870a0b570a2e8
eltrex58 <fgarciaislas.fgi@ciencias.unam.mx>
2026-04-18 23:57:26 -0600

    Ejercicios 1-5 realizados y probados con el test

--- src/Practica04.hs
+++ src/Practica04.hs
@@ -35,24 +35,98 @@ data ArbolDPLL = Node Estado ArbolDPLL | Branch Estado ArbolDPLL ArbolDPLL | Voi
 --IMPLEMENTACION PARTE 1
 --Ejercicio 1
 conflict :: Estado -> Bool
-conflict = undefined
+conflict (_, (x:xs)) = elemento [] (x:xs) 
+
+-- Funcion que verifica si un elemento pertenece a una lista.
+elemento :: Eq a => a -> [a] -> Bool
+elemento _ [] = False
+elemento x (y:ys) = x == y || elemento x ys
 
 --Ejercicio 2
 success :: Estado -> Bool
-success = undefined
+success (_, list) = list == []
 
 --Ejercicio 3
 unit :: Estado -> Estado
-unit = undefined
+unit (interp, clausulas) = buscaLiteral interp clausulas
+
+-- Verifica si en la interpretación no existe el complemento de la literal
+esValida :: Literal -> Interpretacion -> Bool
+esValida _ [] = True
+esValida x (y:ys)  
+    | esComplemento x y = False
+    | otherwise = esValida x ys
+
+-- Verifica si la tupla es complemento de la literal.
+esComplemento :: Literal -> (String, Bool) -> Bool
+esComplemento (Var l) (s, b) = (l == s) && (b == False)
+esComplemento (Not (Var l)) (s,b) = (l == s) && (b == True)
+
+-- Añade la literal a la interpretación. Se supone que no existe complemento en la interpretación.
+anadir :: Literal -> Interpretacion -> Interpretacion
+anadir (Var l) interp = (l, True):interp
+anadir (Not (Var l)) interp = (l, False):interp
+
+-- Busca alguna literal en la lista de cláusulas. Si encuentra, verifica si es valida y de serlo la añade a la interpretación y la elimina de la lista de clausulas.
+-- En caso de no se así, busca de nuevo.
+buscaLiteral :: Interpretacion -> [Clausula] -> Estado
+buscaLiteral interp [] = (interp, [])
+buscaLiteral interp ((y:ys):xs)
+    | (ys == []) && (esValida y interp) = ((anadir y interp), xs)
+    | otherwise = unionTupla ([], [y:ys]) (buscaLiteral interp xs)
+
+-- Une los elementos de dos tuplas de listas a una, las listas se unen como conjunto.
+unionTupla :: (Eq a, Eq b) => ([a],[b]) -> ([a],[b]) -> ([a],[b])
+unionTupla (w,x) (y,z) = ((union w y),(union x z))
+
+-- Une dos listas en una, como conjunto.
+union :: Eq a => [a] -> [a] -> [a]
+union ys [] = ys
+union ys (x:xs)
+    | elemento x ys = union ys xs
+    | otherwise      = union (ys ++ [x]) xs
 
 --Ejercicio 4
 elim :: Estado -> Estado
-elim = undefined
+elim (interp, clausulas) = (interp, eliminacion interp clausulas)
+
+-- Aplica la eliminacion a las clasulas por cada elemento en interpretación.
+eliminacion :: Interpretacion -> [Clausula] -> [Clausula]
+eliminacion [] clausulas = clausulas
+eliminacion ((s,b):xs) clausulas =
+    let l = if b then Var s else Not (Var s)
+    in eliminacion xs (subElim l clausulas)
+
+-- Dada una literal, elimina la clausula que la contenga en toda la lista de clausulas.
+subElim :: Literal -> [Clausula] -> [Clausula]
+subElim _ [] = []
+subElim l (x:xs) 
+    | elemento l x = subElim l xs
+    | otherwise = x:(subElim l xs)
+
 
 --Ejercicio 5
 red :: Estado -> Estado
-red = undefined
+red (interp, clausulas) = (interp, reduccion interp clausulas)
 
+-- Aplica la reducción a las clausulas para cada elemento en interpretación.
+reduccion :: Interpretacion -> [Clausula] -> [Clausula]
+reduccion [] clausulas = clausulas
+reduccion ((s,b):xs) clausulas =
+    let l = if b then Not (Var s) else Var s
+    in reduccion xs (subRed l clausulas)
+
+-- Dada una literal, quita la literal de clasula sobre todas las clausulas.
+subRed :: Literal -> [Clausula] -> [Clausula]
+subRed _ [] = []
+subRed l (x:xs) = (quitaLiteral l x):(subRed l xs)
+
+-- Dada una literal, quita la literal de la clausula.
+quitaLiteral :: Literal -> Clausula -> Clausula
+quitaLiteral _ [] = []
+quitaLiteral l (x:xs)
+    | l == x = quitaLiteral l xs
+    | otherwise = x:(quitaLiteral l xs)
 
 --Ejercicio 6
 sep :: Literal -> Estado -> (Estado, Estado)
